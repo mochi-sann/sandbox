9c869eb2ee171a166abd41462f6b7cbd6bc5aa96
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-10-09 11:06:25 +0900

    update

--- rust-lang/hello-rust/src/main.rs
+++ rust-lang/hello-rust/src/main.rs
@@ -1,7 +1,7 @@
 fn prim_number(num: i32) -> bool {
     let hoge: i32 = num;
     let mut is_prime_number: bool = true;
-    for number in 2..hoge {
+    for number in 2..hoge / 2 {
         // println!("{}", number);
         if hoge % number == 0 {
             is_prime_number = false;
@@ -12,11 +12,22 @@ fn prim_number(num: i32) -> bool {
 }
 
 fn main() {
-    for num in 2..100 {
+    println!("どの数の素数までを計算しますか?");
+    let mut guess = String::new();
+
+    // io::stdin()
+    //     .read_line(&mut guess)
+    //     .expect("行の読み込みに失敗しました"); // 行の読み込みに失敗しました
+    // println!(" input : {}", guess);
+    // let stri: String = String::from(guess);
+    // let serche_number: i32 = stri.parse().unwrap();
+    //
+    for num in 2..1000 {
         if prim_number(num) {
-            println!("{} は素数です", num);
+            print!("{} ", num);
         } else {
-            println!("{} は素数ではありません", num);
+            print!("   ");
+            // println!("{} は素数ではありません", num);
         }
     }
 }
