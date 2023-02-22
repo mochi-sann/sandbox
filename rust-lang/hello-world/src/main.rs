d8454cb0da277fb40e7eef9c7c1c7fc6e81a96ca
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-02-22 10:47:40 +0900

    hoge

--- rust-lang/hello-world/src/main.rs
+++ rust-lang/hello-world/src/main.rs
@@ -1,60 +1,45 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn triangle_size(&self) -> u32 {
-        self.width * self.height / 2
-    }
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-    // otherが自分より大きいかどうか大きかったらtrue 小さかったら false
-    fn is_big(&self, other: &Rectangle) -> bool {
-        self.area() < other.area()
-    }
-}
-
-#[derive(Debug)]
-enum IpAdressKind {
-    V4,
-    V6,
-}
-
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
+    // enum IpAddrKind {
+    //     V4,
+    //     V6,
+    // }
+    #[derive(Debug)]
+    enum IpAddrKind {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4(1, 1, 1, 1),
+        address: String::from("127.0.0.1"),
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
-    println!(
-        "The area of the triangle_size is {} square pixels.",
-        rect1.triangle_size()
-    );
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6("hoge".to_string()),
+        address: String::from("::1"),
     };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
-    let ip_kind = IpAdressKind::V4;
-    println!("{:?}", ip_kind);
+    println!("{:?}", home.kind);
 
-    // rect1にrect2ははまり込む？
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("{:?}", value_in_cents(Coin::Dime));
+}
 
-    println!("is big ? {}", rect1.is_big(&rect3));
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
