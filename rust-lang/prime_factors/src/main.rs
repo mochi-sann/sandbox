b6cab20715c6c5b5cad75a5d490a3afa39cfb3e7
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-05-07 12:31:49 +0900

    WIP

--- rust-lang/prime_factors/src/main.rs
+++ rust-lang/prime_factors/src/main.rs
@@ -1,25 +1,42 @@
 use std::io;
 
-fn prime_factors(mut n: u64) -> Vec<u64> {
-    let mut factors: Vec<u64> = Vec::new();
+fn prime_factors(mut n: u64) -> Vec<(u64, u64)> {
+    let mut factors: Vec<(u64, u64)> = Vec::new();
     let mut i = 2;
+    let mut count = 0;
     while i * i <= n {
-        while n % i == 0 {
-            factors.push(i);
+        if n % i == 0 {
+            if i == factors.last().map(|&x| x.0).unwrap_or(0) {
+                count += 1;
+                let idx = factors.len() - 1;
+                factors[idx].1 = count;
+            } else {
+                count = 1;
+                factors.push((i, count));
+            }
             n /= i;
+        } else {
+            i += 1;
         }
-        i += 1;
     }
     if n > 1 {
-        factors.push(n);
+        factors.push((n, 1));
     }
     factors
 }
+
+
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
     let n: u64 = input.trim().parse().unwrap();
-
+  
     let factors = prime_factors(n);
-    print!("{:?} ", factors);
+    for (factor, count) in factors {
+        if count == 1 {
+            print!("{} ", factor);
+        } else {
+            print!("{}^{} ", factor, count);
+        }
+    }
 }
