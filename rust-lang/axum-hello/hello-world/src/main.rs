4d1cc5e20779c9e031558d6a0690af4689858c38
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-05-08 14:02:05 +0900

    update

---
+++ rust-lang/axum-hello/hello-world/src/main.rs
@@ -0,0 +1,26 @@
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main] // main関数を非同期関数にするために必要
+async fn main() {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "rustwi=hello-world")
+    }
+    // loggingの設定
+
+
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(handler));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<html><h1>Hello, World!</h1></html>")
+}
