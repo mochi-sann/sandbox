eb135abcf11fa61d76079a8c289f5a50754be00d
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-05-09 15:09:23 +0900

    formatした

---
+++ nodejs-ts/my-foods-next-app/src/app/foods/new/page.tsx
@@ -0,0 +1,11 @@
+export default function NewFoodPage() {
+  return (
+    <main>
+      <form>
+        <div>
+          <input type="text" name="name" />
+        </div>
+      </form>
+    </main>
+  );
+}
--- nodejs-ts/my-foods-next-app/src/app/page.tsx
+++ nodejs-ts/my-foods-next-app/src/app/page.tsx
@@ -1,9 +1,22 @@
-import Image from 'next/image'
+import { PrismaClient } from "@prisma/client";
+import Link from "next/link";
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+async function getData() {
+  const foods = await prisma.foods.findMany();
+  console.log({ foods });
+  return foods;
+}
+
+export default async function Home() {
+  const foods = await getData();
   return (
     <main>
       <p>hello world</p>
+      <Link href={"/foods/new"}></Link>
+      <pre>{JSON.stringify(foods, null, 2)}</pre>
+      <table></table>
     </main>
-  )
+  );
 }
