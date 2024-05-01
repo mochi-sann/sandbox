4bce4aab59dfca59d3c5018fa770f6da8966a1b7
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-05-01 16:22:31 +0900

    update

--- nodejs-ts/live-remix-app/app/routes/_index.tsx
+++ nodejs-ts/live-remix-app/app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-
+import { Link } from "@remix-run/react";
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -11,6 +11,12 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <div className="mx-auto mt-16 max-w-7xl text-center">
+        <Link to="/posts" className="text-xl text-blue-600 underline">
+          Blog Posts
+        </Link>
+      </div>
+
       <ul>
         <li>
           <a
