e70375566f49ad5634d8aed70a0f3cbbeb099d5f
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-07-09 09:39:08 +0900

    update

--- ruby/rails-docker/app/controllers/articles_controller.rb
+++ ruby/rails-docker/app/controllers/articles_controller.rb
@@ -6,4 +6,17 @@ class ArticlesController < ApplicationController
   def show
     @articles = Article.find(params[:id])
   end
+
+  def new
+    @articles = Article.new 
+  end 
+  def create 
+    @articles = Article.new(title:"title" , body:"body")
+    if @article.save 
+      redirect_to @article
+    else 
+      render :new, status: unprocessable_entity
+    end
+  end
+
 end
