8e06140ef6ca5b8f4fe117c52051ac534897009a
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-07-11 18:14:08 +0900

    update

--- ruby/rails-docker/db/schema.rb
+++ ruby/rails-docker/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_11_025627) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_11_030559) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -19,10 +19,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_11_025627) do
   end
 
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
+    t.string "comment"
+    t.text "boty"
+    t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_11_025627) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
