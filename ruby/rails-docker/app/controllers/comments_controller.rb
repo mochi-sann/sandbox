8153e29f65092f1ec3723ce4258e04755d37f802
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-07-11 21:39:47 +0900

    update

--- ruby/rails-docker/app/controllers/comments_controller.rb
+++ ruby/rails-docker/app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create 
+    @artcile = Article.find(prams[:article_id])
+    @comment = @article.comments.create(comment_params)
+     respond_to? artcile_path(@artcile)
+  end
+
+  private
+      def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+
 end
