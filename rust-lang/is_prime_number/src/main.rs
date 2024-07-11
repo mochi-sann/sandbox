2d639019f79c1010dcf0dfe3727fa823ecff7ed6
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-07-11 11:53:50 +0900

    update

--- rust-lang/is_prime_number/src/main.rs
+++ rust-lang/is_prime_number/src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+
+fn prime_factorization(mut n: u64) -> Vec<u64> {
+    let mut factors = Vec::new();
+    let mut divisor = 2;
+
+    while n > 1 {
+        if n % divisor == 0 {
+            factors.push(divisor);
+            n /= divisor;
+        } else {
+            divisor += 1;
+        }
+    }
+
+    factors
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let n: u64 = input.trim().parse().expect("有効な数値を入力してください");
+    for n in 1000..1000000 {
+        let factors = prime_factorization(n);
+
+        println!("{}の素因数分解結果：", n);
+        if factors.is_empty() {
+            println!("{}は素数です", n);
+        } else {
+            let result = factors
+                .iter()
+                .map(|&x| x.to_string())
+                .collect::<Vec<String>>()
+                .join(" × ");
+            println!("{}", result);
+        }
+    }
 }
