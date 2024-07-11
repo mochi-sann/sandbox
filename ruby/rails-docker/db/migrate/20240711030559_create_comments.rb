fa33a5b8c9d453d89df9c167500d54bc4ed7a99d
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-07-11 12:06:31 +0900

    update

--- ruby/rails-docker/db/migrate/20240711030559_create_comments.rb
+++ ruby/rails-docker/db/migrate/20240711030559_create_comments.rb
@@ -1,8 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string :commenter
-      t.text :body
+      t.string :comment
+      t.text :boty
+      t.references :article, null: false, foreign_key: true
 
       t.timestamps
     end
