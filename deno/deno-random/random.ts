12dff06c6815103ab7d23e32feeeaf8203a71b47
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-08-31 18:00:14 +0900

    update

--- deno/deno-random/random.ts
+++ deno/deno-random/random.ts
@@ -1,36 +1,51 @@
-function getRandomInt(max) {
+function getRandomInt(max: number) {
   return Math.floor(Math.random() * max);
 }
 
-function countOccurrences<T>(array: T[]): Record<T, number> {
-  const counts: Record<T, number> = {} as Record<T, number>;
+function countOccurrencesAndPercentages<T>(array: T[]): {
+    counts: Record<T, number>,
+    percentages: Record<T, number>
+} {
+    const counts: Record<T, number> = {} as Record<T, number>;
+    const percentages: Record<T, number> = {} as Record<T, number>;
 
-  array.forEach((item) => {
-    counts[item] = (counts[item] || 0) + 1;
-  });
+    array.forEach((item) => {
+        counts[item] = (counts[item] || 0) + 1;
+    });
 
-  return counts;
+    const total = array.length;
+
+    for (const key in counts) {
+        percentages[key] = (counts[key] / total) * 100;
+    }
+
+    return { counts, percentages };
 }
 
+
+const TOTAL = 10000000
+const RANDOM_COUNT = 10
 function main() {
   // console.log(getRandomInt(6));
 
   let rdm_numbers: number[] = [];
 
-  for (let index = 0; index < 600; index++) {
-    const rdm_int = getRandomInt(6)
+  for (let index = 0; index < TOTAL; index++) {
+    const rdm_int = getRandomInt(RANDOM_COUNT)
     rdm_numbers.push(rdm_int);
     if (rdm_int == 0) {
 
     }
 
   }
-  const rdm_numbers_occ = countOccurrences(rdm_numbers);
+  const rdm_numbers_occ = countOccurrencesAndPercentages(rdm_numbers);
   console.log(rdm_numbers_occ);
+
+  // console.log(rdm_numbers_occ.percentages);
 
 }
 
-for (let index = 0; index < 10; index++) {
+for (let index = 0; index < 1; index++) {
   main();
 
 }
