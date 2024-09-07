120a79bcefdf104cbba884134a876bd49f5a21cb
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-09-07 10:35:59 +0900

    update

--- ruby/vite_rails_app/app/javascript/entrypoints/application.jsx
+++ ruby/vite_rails_app/app/javascript/entrypoints/application.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
 // To see this message, add the following to the `<head>` section in your
 // views/layouts/application.html.erb
 //
@@ -5,11 +7,41 @@
 //    <%= vite_javascript_tag 'application' %>
 console.log('Vite ⚡️ Rails')
 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
 // If using a TypeScript entrypoint file:
 //     <%= vite_typescript_tag 'application' %>
 //
 // If you want to use .jsx or .tsx, add the extension:
 //     <%= vite_javascript_tag 'application.jsx' %>
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 
 console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
 
--- ruby/vite_rails_app/vite.config.ts
+++ ruby/vite_rails_app/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import FullReload from 'vite-plugin-full-reload'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 100 }),
+    react()
+
   ],
 })
