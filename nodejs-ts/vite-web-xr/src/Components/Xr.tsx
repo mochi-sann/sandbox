dd9696de91461de2d99a7915ea0a1c1820406780
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-10-08 16:42:12 +0900

    update

--- nodejs-ts/vite-web-xr/src/Components/Xr.tsx
+++ nodejs-ts/vite-web-xr/src/Components/Xr.tsx
@@ -2,19 +2,27 @@
 
 import { Sphere } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { VRButton, XR } from "@react-three/xr";
+
 
 export  function Xr() {
   return (
-    <main className="container mx-auto">
+        <main className="container mx-auto">
       <h1 className="mt-4 mb-4 text-4xl">React Three XR</h1>
+      <div className="mb-4">
+        <VRButton></VRButton>
+      </div>
       <Canvas className="aspect-video border border-gray-300">
-        <Sphere>
-          <ambientLight intensity={0.1}></ambientLight>
-          <pointLight intensity={0.8} position={[10, 10, 10]}></pointLight>
-          <meshStandardMaterial color="#fafafa"></meshStandardMaterial>
-        </Sphere>
+        <XR>
+          <Sphere>
+            <ambientLight intensity={0.1}></ambientLight>
+            <pointLight intensity={0.8} position={[10, 10, 10]}></pointLight>
+            <meshStandardMaterial color="#fafafa"></meshStandardMaterial>
+          </Sphere>
+        </XR>
       </Canvas>
     </main>
+
   );
 }
 
