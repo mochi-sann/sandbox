2ce8dc7847a5c0e78bd2868beef7b9220acbf74d
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-10-25 21:38:54 +0900

    update

--- nodejs-ts/my-hono-typed-sql/src/index.ts
+++ nodejs-ts/my-hono-typed-sql/src/index.ts
@@ -1,16 +1,21 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { PrismaClient } from "@prisma/client";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+app.get("/users", (c) => {
+  const prisma = new PrismaClient()
+  return c.json({ hoge: "hogehoge" });
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
