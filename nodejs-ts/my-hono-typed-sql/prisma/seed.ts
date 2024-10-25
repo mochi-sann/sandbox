2725830e967c85ce9396a389c80c5ed81bd47793
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-10-25 22:09:52 +0900

    update

--- nodejs-ts/my-hono-typed-sql/prisma/seed.ts
+++ nodejs-ts/my-hono-typed-sql/prisma/seed.ts
@@ -1,46 +1,43 @@
 import { PrismaClient, type Post } from "@prisma/client";
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
 async function main() {
+	// Create 10 users with random data
+	for (let i = 0; i < 10; i++) {
+		const posts = [];
+		const randomTimes = Math.floor(Math.random() * (10000 + 1));
 
-  // Create 10 users with random data
-  for (let i = 0; i < 10; i++) {
-    const posts = []
-    const randomTimes = Math.floor(Math.random() * (10000 + 1));
+		console.log(`Looping ${randomTimes} times`);
 
-    console.log(`Looping ${randomTimes} times`);
-
-    // ランダムな回数分ループ
-    for (let i = 0; i < randomTimes; i++) {
-      posts.push({
-        title: faker.lorem.sentence(),
-        content: faker.lorem.paragraph(),
-        published: faker.datatype.boolean(),
-      },
-      )
-    }
-    const user = await prisma.user.create({
-      data: {
-        email: faker.internet.email(),
-        name: faker.person.firstName(),
-        posts: {
-          create: posts,
-        },
-      },
-    });
-    console.log(`Created user with id: ${user.id}`);
-  }
+		// ランダムな回数分ループ
+		for (let i = 0; i < randomTimes; i++) {
+			posts.push({
+				title: faker.lorem.sentence(),
+				content: faker.lorem.paragraph(),
+				published: faker.datatype.boolean(),
+			});
+		}
+		const user = await prisma.user.create({
+			data: {
+				email: faker.internet.email(),
+				name: faker.person.firstName(),
+				posts: {
+					create: posts,
+				},
+			},
+		});
+		console.log(`Created user with id: ${user.id}`);
+	}
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
-
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
--- nodejs-ts/my-hono-typed-sql/src/index.ts
+++ nodejs-ts/my-hono-typed-sql/src/index.ts
@@ -4,27 +4,27 @@ import { getUsersWithPosts } from "@prisma/client/sql";
 import { Hono } from "hono";
 
 const app = new Hono();
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+	return c.text("Hello Hono!");
 });
-app.get("/users", async (c) => {
-  const usersWithPostCounts = await prisma.$queryRawTyped(getUsersWithPosts())
-  let users = usersWithPostCounts.map(value => ({
-    postCount: Number(value.postCount)
-    , name: value.name, id: value.id
+app.get("/users/postcount", async (c) => {
+	const usersWithPostCounts = await prisma.$queryRawTyped(getUsersWithPosts());
+	const users = usersWithPostCounts.map((value) => ({
+		postCount: Number(value.postCount),
+		name: value.name,
+		id: value.id,
+	}));
+	console.log(...[users, "👀 [index.ts:19]: users"].reverse());
 
-  }))
-  console.log(...[users, '👀 [index.ts:19]: users'].reverse());
-
-  return c.json(users);
+	return c.json(users);
 });
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
 
 serve({
-  fetch: app.fetch,
-  port,
+	fetch: app.fetch,
+	port,
 });
