c54874bdd5e4e0503d90d1e92a53a919c8bf5e69
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-12-05 19:18:34 +0900

    update

--- nodejs-ts/hono-prisma-view/src/index.ts
+++ nodejs-ts/hono-prisma-view/src/index.ts
@@ -1,10 +1,27 @@
 import { serve } from "@hono/node-server";
+import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
 
 const app = new Hono();
+const prisma = new PrismaClient()
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+app.get("/todos", async  (c) => {
+  const todos = await prisma.todo.findMany();
+  console.log(...[todos, '👀 [index.ts:13]: todos'].reverse());
+  return c.json(todos);
+});
+app.get("/todos-users", async  (c) => {
+  const todos = await prisma.todoTag.findMany();
+  console.log(...[todos, '👀 [index.ts:13]: todos'].reverse());
+  return c.json(todos);
+});
+app.get("/todos/view", async  (c) => {
+  const todos = await prisma.todo_list_with_user.findMany();
+  console.log(...[todos, '👀 [index.ts:13]: todos'].reverse());
+  return c.json(todos);
 });
 
 const port = 3000;
