65f89522312e4de84c9db092706ee41e44a71c07
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-12-10 19:18:29 +0900

    add adonijjs

---
+++ nodejs-ts/my-adonisjs-app/start/routes.ts
@@ -0,0 +1,21 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.get('/', async () => {
+  return {
+    hello: 'world',
+  }
+})
+router.get('/hoge', async () => {
+  return {
+    hello: 'hoge',
+  }
+})
