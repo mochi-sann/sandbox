95df19d6719d23f91e1f94e14ef3e9c262d8810c
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2024-12-11 13:03:11 +0900

    update

--- nodejs-ts/nestia-new/postgres.sh
+++ nodejs-ts/nestia-new/postgres.sh
@@ -1,4 +1,4 @@
 docker pull postgres:latest
 docker ps | grep postgres > /dev/null || docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=root -d postgres:latest
-npm run build:prisma
-npm run schema
+pnpm run build:prisma
+pnpm run schema
