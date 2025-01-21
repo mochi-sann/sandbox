c9bfb7396fdc9504d164a01fb5dfa3b23251fbb0
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-01-21 17:56:15 +0900

    update

--- go/web-todo/main.go
+++ go/web-todo/main.go
@@ -1,8 +1,8 @@
-//	@title			Todo API
-//	@version		1.0
-//	@description	This is a sample Todo API.
-//	@host			localhost:8080
-//	@BasePath		/
+// @title			Todo API
+// @version		1.0
+// @description	This is a sample Todo API.
+// @host			localhost:8080
+// @BasePath		/
 package main
 
 import (
@@ -45,22 +45,22 @@ func init() {
 	db.AutoMigrate(&Todo{})
 }
 
-//	@Summary		Get all todos
-//	@Description	Get all todos
-//	@Produce		json
-//	@Success		200	{array}	Todo
-//	@Router			/todos [get]
+// @Summary		Get all todos
+// @Description	Get all todos
+// @Produce		json
+// @Success		200	{array}	Todo
+// @Router			/todos [get]
 func getTodos(c *gin.Context) {
 	var todos []Todo
 	db.Find(&todos)
 	c.JSON(http.StatusOK, todos)
 }
 
-//	@Summary		Ping
-//	@Description	Ping
-//	@Produce		json
-//	@Success		200	{object}	map[string]string
-//	@Router			/ping [get]
+// @Summary		Ping
+// @Description	Ping
+// @Produce		json
+// @Success		200	{object}	map[string]string
+// @Router			/ping [get]
 func ping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "hoge",
@@ -68,14 +68,14 @@ func ping(c *gin.Context) {
 
 }
 
-//	@Summary		Create a new todo
-//	@Description	Create a new todo
-//	@Accept			json
-//	@Produce		json
-//	@Param			todo	body		Todo	true	"Todo object"
-//	@Success		201		{object}	Todo
-//	@Failure		400		{object}	map[string]string
-//	@Router			/todos [post]
+// @Summary		Create a new todo
+// @Description	Create a new todo
+// @Accept			json
+// @Produce		json
+// @Param			todo	body		Todo	true	"Todo object"
+// @Success		201		{object}	Todo
+// @Failure		400		{object}	map[string]string
+// @Router			/todos [post]
 func createTodo(c *gin.Context) {
 	var todo Todo
 	if err := c.ShouldBindJSON(&todo); err != nil {
@@ -86,14 +86,13 @@ func createTodo(c *gin.Context) {
 	c.JSON(http.StatusCreated, todo)
 }
 
-
-//	@Summary		Get a todo by ID
-//	@Description	Get a todo by ID
-//	@Produce		json
-//	@Param			id	path		int	true	"Todo ID"
-//	@Success		200	{object}	Todo
-//	@Failure		404	{object}	map[string]string
-//	@Router			/todos/{id} [get]
+// @Summary		Get a todo by ID
+// @Description	Get a todo by ID
+// @Produce		json
+// @Param			id	path		int	true	"Todo ID"
+// @Success		200	{object}	Todo
+// @Failure		404	{object}	map[string]string
+// @Router			/todos/{id} [get]
 func getTodo(c *gin.Context) {
 	id := c.Param("id")
 	var todo Todo
@@ -104,16 +103,16 @@ func getTodo(c *gin.Context) {
 	c.JSON(http.StatusOK, todo)
 }
 
-//	@Summary		Update a todo by ID
-//	@Description	Update a todo by ID
-//	@Accept			json
-//	@Produce		json
-//	@Param			id		path		int		true	"Todo ID"
-//	@Param			todo	body		Todo	true	"Todo object"
-//	@Success		200		{object}	Todo
-//	@Failure		400		{object}	map[string]string
-//	@Failure		404		{object}	map[string]string
-//	@Router			/todos/{id} [put]
+// @Summary		Update a todo by ID
+// @Description	Update a todo by ID
+// @Accept			json
+// @Produce		json
+// @Param			id		path		int		true	"Todo ID"
+// @Param			todo	body		Todo	true	"Todo object"
+// @Success		200		{object}	Todo
+// @Failure		400		{object}	map[string]string
+// @Failure		404		{object}	map[string]string
+// @Router			/todos/{id} [put]
 func updateTodo(c *gin.Context) {
 	id := c.Param("id")
 	var todo Todo
@@ -129,13 +128,13 @@ func updateTodo(c *gin.Context) {
 	c.JSON(http.StatusOK, todo)
 }
 
-//	@Summary		Delete a todo by ID
-//	@Description	Delete a todo by ID
-//	@Produce		json
-//	@Param			id	path		int	true	"Todo ID"
-//	@Success		200	{object}	map[string]string
-//	@Failure		404	{object}	map[string]string
-//	@Router			/todos/{id} [delete]
+// @Summary		Delete a todo by ID
+// @Description	Delete a todo by ID
+// @Produce		json
+// @Param			id	path		int	true	"Todo ID"
+// @Success		200	{object}	map[string]string
+// @Failure		404	{object}	map[string]string
+// @Router			/todos/{id} [delete]
 func deleteTodo(c *gin.Context) {
 	id := c.Param("id")
 	var todo Todo
