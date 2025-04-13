8c79964cb98b454b9f646d55a99432350b69bfa3
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-04-13 09:45:27 +0900

    update

--- nodejs-ts/vite-park-ui/panda.config.ts
+++ nodejs-ts/vite-park-ui/panda.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "@pandacss/dev";
-
+import { createPreset } from "@park-ui/panda-preset";
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
+  presets: [    createPreset({ accentColor: neutral, grayColor: neutral, radius: 'sm' }),],
   // Where to look for your css declarations
   include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
 
