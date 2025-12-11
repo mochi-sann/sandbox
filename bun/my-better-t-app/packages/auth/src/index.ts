4367895a046fd446c63ea4c3f3b2172846317281
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2025-12-11 22:40:31 +0900

    init

---
+++ bun/my-better-t-app/packages/auth/src/index.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@my-better-t-app/db";
+import * as schema from "@my-better-t-app/db/schema/auth";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+
+		schema: schema,
+	}),
+	trustedOrigins: [process.env.CORS_ORIGIN || ""],
+	emailAndPassword: {
+		enabled: true,
+	},
+	advanced: {
+		defaultCookieAttributes: {
+			sameSite: "none",
+			secure: true,
+			httpOnly: true,
+		},
+	},
+});
