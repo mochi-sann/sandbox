c1585a135d5e25b705582881420c6207ba272caf
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2026-02-28 12:48:27 +0900

    update

--- threejs/threejs-vite-template/src/components/debug/PerfPanel.tsx
+++
@@ -1,7 +0,0 @@
-import { Perf } from 'r3f-perf'
-
-export function PerfPanel() {
-  if (!import.meta.env.DEV) return null
-
-  return <Perf position="top-left" />
-}
--- threejs/threejs-vite-template/src/components/scene/Lights.tsx
+++
@@ -1,8 +0,0 @@
-export function Lights() {
-  return (
-    <>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[4, 8, 4]} intensity={1.1} castShadow />
-    </>
-  )
-}
